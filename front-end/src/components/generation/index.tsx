e1a967f65850bf1375c0cbf318c5187f3cffc1ec
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2021-04-30 04:57:24 +0700

    feat(generation): display generated text on meme
    allow editing

--- front-end/src/components/generation/index.tsx
+++ front-end/src/components/generation/index.tsx
@@ -1,6 +1,8 @@
-import { useState, useEffect, useRef, forwardRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { exportComponentAsJPEG } from 'react-component-export-image'
+import GetAppIcon from '@material-ui/icons/GetApp';
+import CachedIcon from '@material-ui/icons/Cached';
 
 import { MemeTemplate } from '../../types/meme'
 import { getTemplateById } from '../../services/meme'
@@ -24,14 +26,43 @@ function Generation() {
         })()
     }, [id])
 
+    const handleInput = (e: any) => {
+        setMemeText(e.target.value)
+    }
+
     if (template) return (
         <div className={styles.generation}>
-            <div className={styles.imageColumn}>
-                <Meme template={template} text={memeText} ref={memeRef}/>
-                <button onClick={()=>{exportComponentAsJPEG(memeRef)}}>download</button>
-            </div>            
-            <div className={styles.textColumn}>
-
+            <div className={styles.flexContainer}>
+                <div className={styles.columnCenter}>
+                    <Meme template={template} text={memeText} ref={memeRef}/>
+                </div>            
+                <div className={styles.column}>
+                    <div className={styles.memeLabel}>{template.label}</div>
+                    <div className={styles.inputLabel}>Generated text :</div>
+                    <textarea
+                        className={styles.textarea}
+                        value={memeText}
+                        spellCheck='false'
+                        onChange={handleInput}
+                    />
+                </div>
+            </div>
+            <div className={styles.buttonRow}>
+                <div className={styles.columnCenter}>
+                    <button
+                        onClick={()=>{exportComponentAsJPEG(memeRef)}}
+                        className={styles.button}
+                    >
+                        <GetAppIcon/>
+                        <span className={styles.buttonText}>download</span>
+                    </button>
+                </div>
+                <div className={styles.columnCenter}>
+                    <button className={styles.buttonSecondary}>
+                        <CachedIcon/>
+                        <span className={styles.buttonText}>generate again</span>
+                    </button>
+                </div>
             </div>
         </div>
     )
