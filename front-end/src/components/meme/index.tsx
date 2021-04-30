a6d1f15b24f0c1a951d58947382fb1c97e0dafba
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2021-04-30 23:08:24 +0700

    feat(generation): fit text in each template text box

--- front-end/src/components/meme/index.tsx
+++ front-end/src/components/meme/index.tsx
@@ -1,4 +1,6 @@
 import { forwardRef } from 'react'
+// @ts-ignore
+import { Textfit } from 'react-textfit'
 
 import styles from './style.module.css'
 import { MemeTemplate } from '../../types/meme'
@@ -11,11 +13,32 @@ interface MemeProps {
 const Meme = forwardRef<HTMLDivElement, MemeProps>(({template, text}, ref) => {
     const splitText = text.split('<SEP>')
 
+    const pickStyle = (id: number) => {
+        switch(id) {
+            case 0:
+                return [styles.textTopRight, styles.textBottomRight]
+            case 70:
+                return [styles.textTopLeft, styles.textBottomLeft]
+            case 8:
+                return [styles.textHighLeft, styles.textHighRight]
+            case 25:
+                return [styles.trumpLeft, styles.trumpRight]
+            case 32:
+                return [styles.rockTop, styles.rockMiddle]
+            case 28:
+                return [styles.inScroll, styles.outScroll]
+            default:
+                return [styles.textTop, styles.textBottom]
+        } 
+    }
+
+    const textStyle = pickStyle(template.id)
+
     return (
         <div className={styles.meme} ref={ref}>
             <img src={`data:image/jpg;base64,${template.imageBinary}`} alt={template.label}/>
-            <div className={styles.text1}>{splitText[0]}</div>
-            <div className={styles.text2}>{splitText[1]}</div>
+            <Textfit className={textStyle[0]} mode="multi">{splitText[0]}</Textfit>
+            <Textfit className={textStyle[1]}>{splitText[1]}</Textfit>
         </div>
     )
 });
